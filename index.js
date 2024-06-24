7fed08c68c6fdadeff7ee0aeddb064c9dc4ffd0f
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-24 19:45:18 +0800

    Make the forms for login and signup

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const loginButton = document.querySelector('.login-button')
 const signupButton = document.querySelector('.signup-button');
+const login = `<h1>You clicked the Log in button</h1> 
+               <form action="./index.php">
+                  username: <br>
+                  <input type="text" name="username"> <br>
+                  password: <br>
+                  <input type="password" name="password"> <br>
+                  <input type="submit" name="login" value="Log in">
+               </form>`
 
+const signup = `<h1>You clicked the Sign up button</h1> 
+                <form action="./index.php">
+                  username: <br>
+                  <input type="text" name="username"> <br>
+                  password: <br>
+                  <input type="password" name="password"> <br>
+                  re-enter password: <br>
+                  <input type="password" name="reEnterPassword"> <br>
+                  <input type="submit" name="signup" value="Sign up">
+               </form>`
 
 loginButton.addEventListener("click", () => {
-  document.body.innerHTML = "<h1>You clicked the Log in button</h1>"
+  document.body.innerHTML = login
 })
 
 
 signupButton.addEventListener("click", () => {
-  document.body.innerHTML = "<h1>You clicked the Sign up button</h1>"
+  document.body.innerHTML = signup
 })
