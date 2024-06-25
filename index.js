bf040e67c88199933962e02c187c1b682ee15e17
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-25 16:40:18 +0800

    Create login.php and signup.php

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const login = `<h1>You clicked the Log in button</h1>
                   password: <br>
                   <input type="password" name="password"> <br>
                   <input type="submit" name="login" value="Log in">
-               </form>`
+               </form>
+               <div>Don't have an account? <a href="">Signup</a></div>`
 
 const signup = `<h1>You clicked the Sign up button</h1> 
                 <form action="./index.php">
