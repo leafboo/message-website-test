d87bd8920331db39ac53c1c43fd42c91712ded06
Jan <110758056+leafboo@users.noreply.github.com>
2024-06-29 14:30:44 +0800

    Remove the unnecessary code in index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <script src="./index.js" defer></script>
 </head>
 <body>
-  <a href="./home-page.php"><button class="login-button">Log in</button></a>
+  <a href="./login.php"><button class="login-button">Log in</button></a>
   <a href="./signup.php"><button class="signup-button">Sign up</button></a>
 </body>
 </html>
@@ -18,29 +18,5 @@
 // REVIEW THIS CODE, IDK HOW IT ALL WORKS
 // sanitize removes the undesired shit
 // validate returns an empty string if it doesn't pass validation
-  session_start();
-
-  if(!isset($_SESSION['messages'])) {
-    $_SESSION['messages'] = [];
-  }
-
-  if(isset($_POST['submit'])) {
-    //$new_message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
-    //$_SESSION['messages'][] = $new_message;
-    echo $_POST["message"];
-  }
-
-   //echo '<pre>' . print_r($_POST, true) . '</pre>';
-
-   //foreach($_POST as $key=>$value) {
-    //echo "{$key} => {$value} <br>";
-
-   //}
-  
-
-  //if (!empty($_SESSION['messages'])) {
-    //foreach($_SESSION['messages'] as $message) {
-      //echo "{$message} <br>";
-    //}
-  //}
+ 
 ?>
